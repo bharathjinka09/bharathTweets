b1fd0fd9fb46d8025b288b3c17e0aef13a71679f
bharathjinka09 <bharathjinka09@gmail.com>
2020-06-12 22:18:04 +0530

    added

--- django_project/settings.py
+++ django_project/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'django_project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR,'db.sqlite3'),
     }
 }
 
